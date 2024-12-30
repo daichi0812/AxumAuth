ce7ed74f4d56feacc17082797cecc5fa2dbe5495
Daichi <a5822108@aoyama.jp>
2024-12-30 16:29:52 +0900

    7: 1:35:15

--- src/models.rs
+++ src/models.rs
@@ -32,7 +32,7 @@ pub struct User {
     pub verification_token: Option<String>, 
     pub token_expires_at: Option<DateTime<Utc>>, 
     #[serde(rename = "createdAt")] // JSONシリアライズ時のキー名を "createdAt" に変更
-    pub created_at: DateTime<Utc>, 
+    pub created_at: Option<DateTime<Utc>>, 
     #[serde(rename = "updatedAt")] // JSONシリアライズ時のキー名を "updatedAt" に変更
-    pub updated_at: DateTime<Utc>, 
+    pub updated_at: Option<DateTime<Utc>>, 
 }
